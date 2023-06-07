9b5d3fa5072216f610d897236a18a3378ad45656
Seif Atalla <s4hk@yahoo.com>
2023-06-07 20:53:24 +0300

    new api key

--- src/components/Api/interceptor.ts
+++ src/components/Api/interceptor.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://api.apilayer.com/fixer/',
   headers: {
+    apiKey: 'LJu485HTyl7FinIA9pd2FAYLixp4xmgv',
     // apiKey: '4TwxRkNqwq5N6w6ZgKbgEn1pBrxUd75p',
-    // apiKey: 'CVuffhwQpqQiR4sCgKBcwDFtQWhjoaJi',
   },
 });
 
